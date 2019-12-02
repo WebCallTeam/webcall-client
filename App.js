9c7e203a9509644d47361c61924c5b39da2820ac
vios276 <tw01151@gmail.com>
2019-12-02 16:41:13 +0900

    test

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to ddd start working on your app!</Text>
-      <Button title="Change text" />
+      <Text style={styles.text}>Hello!!</Text>
     </View>
   );
 }
@@ -13,8 +12,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: "white",
+    fontSize: 30
   }
 });
