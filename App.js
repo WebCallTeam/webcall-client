b1b8bc56345e2b7ca4129bc302cc96e2dd5662cf
Redrock912 <leset912@gmail.com>
2019-12-07 21:00:17 +0900

    mobx 적용 완료

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import MainScreen from './Components/MainScreen';
+import React, { Component } from "react";
 
-const AppStackNavigator = createStackNavigator({
-  Main:{
-    screen: MainScreen 
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import MainScreen from "./src/Components/MainScreen";
+import { Provider } from "mobx-react";
+import Store from "./src/store/Store";
+
+export default class App extends Component {
+  render() {
+    const AppStackNavigator = createStackNavigator({
+      Main: {
+        screen: MainScreen
+      }
+    });
+
+    const AppContainer = createAppContainer(AppStackNavigator);
+
+    return (
+      <Provider Store={Store}>
+        <AppContainer />
+      </Provider>
+    );
   }
-});
-
-export default createAppContainer(AppStackNavigator);
+}
---
+++ src/Components/MainScreen.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Platform } from "react-native";
+import { createMaterialTopTabNavigator } from "react-navigation-tabs";
+import { createAppContainer } from "react-navigation";
+
+import HomeTab from "./TabNavigator/HomeTab";
+import OrderListTab from "./TabNavigator/OrderListTab";
+import ProfileTab from "./TabNavigator/ProfileTab";
+
+const AppTabNavigator = createMaterialTopTabNavigator(
+  {
+    HomeTab: { screen: HomeTab },
+    OrderListTab: { screen: OrderListTab },
+    ProfileTab: { screen: ProfileTab }
+  },
+  {
+    animationEnabled: true,
+    swipeEnabled: true,
+    tabBarPosition: "bottom",
+    tabBarOptions: {
+      style: {
+        ...Platform.select({
+          ios: {
+            backgroundColor: "white"
+          },
+          android: {
+            backgroundColor: "white"
+          }
+        })
+      },
+      iconStyle: { height: 30 },
+      activeTintColor: "#000",
+      inactiveTintColor: "#d1cece",
+      upperCaseLabel: false,
+      showLabel: false,
+      showIcon: true,
+      indicatorStyle: { backgroundColor: "#ffffff" }
+    }
+  }
+);
+
+const AppTabContainet = createAppContainer(AppTabNavigator);
+
+export default class MainScreen extends Component {
+  static navigationOptions = {
+    title: (name = "WEBCALL"),
+    headerTitleStyle: { alignSelf: "center", textAlign: "center", flex: 1 }
+  };
+
+  render() {
+    return <AppTabContainet />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
