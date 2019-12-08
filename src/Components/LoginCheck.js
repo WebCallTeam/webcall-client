ba8dc7d1b024d1e66dffb4141f7b71276f34d065
Redrock912 <leset912@gmail.com>
2019-12-08 23:06:12 +0900

    revive logincheck

---
+++ src/Components/LoginCheck.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ActivityIndicator, AsyncStorage, StatusBar, StyleSheet, View, Text } from 'react-native';
+
+class LoginCheck extends React.Component {
+  componentDidMount() {
+    this._bootstrapAsync();
+  }
+
+  _bootstrapAsync = async () => {
+    const userToken = await AsyncStorage.getItem('userToken');
+    this.props.navigation.navigate(userToken ? 'App' : 'Auth');
+  };
+
+  render() {
+    return (
+      <View>
+        <ActivityIndicator />
+        <StatusBar barStyle="default" />
+      </View>
+    );
+  }
+}
+
+export default LoginCheck;
