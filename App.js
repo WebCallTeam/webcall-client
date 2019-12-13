5a76896b5b9c29f019eb009dda2f7ff82e044a7b
Redrock912 <leset912@gmail.com>
2019-12-13 17:58:14 +0900

    file upload

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { createSwitchNavigator, createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-
+import React, { Component } from "react";
 import LoginCheck from "./src/Components/LoginCheck";
 import { LoginScreen, MainScreen, SignInScreen } from "./src/Screen";
+import { Provider } from "mobx-react";
+import { userInfo } from "./src/store";
 
 const AppStack = createStackNavigator({ Main: MainScreen });
 const AuthStack = createStackNavigator({
@@ -10,7 +12,7 @@ const AuthStack = createStackNavigator({
   SignIn: SignInScreen
 });
 
-export default createAppContainer(
+const AppContainer = createAppContainer(
   createSwitchNavigator(
     {
       Loading: LoginCheck,
@@ -22,3 +24,26 @@ export default createAppContainer(
     }
   )
 );
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider userInfo={userInfo}>
+        <AppContainer />
+      </Provider>
+    );
+  }
+}
+
+// export default createAppContainer(
+//   createSwitchNavigator(
+//     {
+//       Loading: LoginCheck,
+//       App: AppStack,
+//       Auth: AuthStack
+//     },
+//     {
+//       initialRouteName: "Loading"
+//     }
+//   )
+// );
---
+++ src/store/index.js
@@ -0,0 +1,5 @@
+import UserInfo from "./UserInfo";
+
+// 추후에 더 추가 될 수도 있음
+const userInfo = new UserInfo();
+export { userInfo };
