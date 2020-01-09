78ff7df62cc9ad008208662cdec0a91c4c8aaa0a
Redrock912 <leset912@gmail.com>
2020-01-09 17:46:59 +0900

    주문번호 지정 후 다시 서버에 보내는 로직

--- src/Components/OrderBox.js
+++ src/Components/OrderBox.js
@@ -17,36 +17,35 @@ class OrderBox extends Component {
     //this.getOrderData();
   }
 
+  // 주문번호는 local에서 관리 , mobx 사용안함
   state = {
     orderData: ""
   };
 
   // 주문 완료 버튼
-  callCustomer = () => {
+  callCustomerWhenDone = () => {
     // 점장의 경우기 때문에
     // 점장이 가지고 있는 리스트에 저장되 있는 token 정보를 읽어오는 로직
-    // const {userInfo} = this.props;
-    // const {orderInfo} = this.props;
-    // try {
-    //   let response = await fetch(PUSH_ENDPOINT + userInfo.id, {
-    //     method: "POST",
-    //     headers: {
-    //       Accept: "application/json",
-    //       "Content-Type": "application/json"
-    //     },
-    //     body: JSON.stringify({
-    //       number: orderInfo.orderNumber,
-    //       password: "default",
-    //       expo_token: token
-    //     })
-    //   });
-    //   let responseJson = await response.json();
-    //   this.setNotification(responseJson.data);
-    //   //console.log(responseJson);
-    // } catch (err) {
-    //   console.log(err);
-    // }
-    // order를 마무리 짓는 로직이 완성되면 그 때 이 로직을 보내기로
+    const {userInfo} = this.props;
+    try {
+      let response = await fetch(PUSH_ENDPOINT + userInfo.id, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          number: this.state.orderData,
+          expo_token: token
+        })
+      });
+
+      //확인 로직 
+      // let responseJson = await response.json();
+      // console.log(responseJson);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   deleteList = value => {
@@ -57,10 +56,10 @@ class OrderBox extends Component {
     return (
       <View style={styles.elem}>
         <View style={styles.userInfo}>
-          <Text style={styles.name}>{this.props.index}번 주문</Text>
+          <Text style={styles.name}>{this.state.orderData ? this.state.orderData + " 번 주문" : "주문 번호를 지정해주세요"}</Text>
         </View>
         <View style={styles.userComment}>
-          <TouchableOpacity style={styles.button} onPress={this.callCustomer}>
+          <TouchableOpacity style={styles.button} onPress={this.callCustomerWhenDone}>
             <Text>확인</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.button} onPress={this.deleteList}>
