08af9c11a8a7956f27307b8a805fdb0e08b99d55
vios276 <tw01151@gmail.com>
2020-01-21 02:23:08 +0900

    QR Generater in HomeTab

--- src/TabNavigator/HomeTab.js
+++ src/TabNavigator/HomeTab.js
@@ -15,19 +15,23 @@ export default class HomeTab extends Component {
     )
   };
   state = {
-    StartQR: false
+    QRState: 0
   };
 
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <NavigationEvents
-          onWillBlur={() => this.setState({ StartQR: false })}
-        />
-        {this.state.StartQR ? (
+        <NavigationEvents onWillBlur={() => this.setState({ QRState: 0 })} />
+        {this.state.QRState === 1 ? (
           <View style={{ flex: 1 }}>
             <View style={{ height: 30 }} />
             <QRScan />
+            <View style={{ height: 30 }} />
+          </View>
+        ) : this.state.QRState === 2 ? (
+          <View style={{ flex: 1 }}>
+            <View style={{ height: 30 }} />
+            <QRMake />
             <View style={{ height: 30 }} />
           </View>
         ) : (
@@ -37,7 +41,7 @@ export default class HomeTab extends Component {
               height: hp("5%")
             }}
           >
-            <View style={{ height: 30 }} />
+            <View style={{ height: 30, flexDirection: "row" }} />
             <TouchableOpacity
               style={{
                 backgroundColor: "#46c3ad",
@@ -46,9 +50,21 @@ export default class HomeTab extends Component {
                 justifyContent: "center",
                 alignItems: "center"
               }}
-              onPress={() => this.setState({ StartQR: true })}
+              onPress={() => this.setState({ QRState: 1 })}
             >
               <Text style={{ color: "white" }}>QR코드인식</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                backgroundColor: "#46c3ad",
+                width: "100%",
+                height: "100%",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+              onPress={() => this.setState({ QRState: 2 })}
+            >
+              <Text style={{ color: "white" }}>QR코드생성</Text>
             </TouchableOpacity>
             <View style={{ height: 30 }} />
           </View>
