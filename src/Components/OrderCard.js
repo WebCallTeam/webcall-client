da86927ede7e1ce5526dd7d63d6bfa4ef310aed5
vios276 <tw01151@gmail.com>
2020-04-12 18:17:37 +0900

    OrderList 메시지 수정

--- src/Components/OrderCard.js
+++ src/Components/OrderCard.js
@@ -5,7 +5,7 @@ import { inject, observer } from "mobx-react";
 
 class OrderCard extends Component {
   state = {
-    dataOrigin: userInfo.notification.data
+    dataOrigin: userInfo.notification.data,
   };
 
   render() {
@@ -13,24 +13,23 @@ class OrderCard extends Component {
       <View
         style={{
           flex: 1,
-          width: "100%",
           alignItems: "center",
           alignSelf: "center",
           justifyContent: "center",
-          padding: 5
+          padding: 5,
         }}
       >
         <View
           style={{
             flex: 1,
-            justifyContent: "center"
+            justifyContent: "center",
           }}
         >
           <Text
             style={{
               alignItems: "center",
               justifyContent: "center",
-              fontSize: 30
+              fontSize: 30,
             }}
           >
             당신의 주문정보
@@ -39,16 +38,19 @@ class OrderCard extends Component {
         <View
           style={{
             flex: 4,
-            justifyContent: "flex-start"
+            alignItems: "center",
+            justifyContent: "center",
+            flexWrap: "wrap",
           }}
         >
           <Text
             style={{
-              fontSize: 40
+              fontSize: 30,
+              width: "100%",
             }}
           >
             {!userInfo.notification.data.number
-              ? "\n\nQRCode를 사용하여\n주문을 해주세요"
+              ? "QRCode를 인식해주세요!"
               : userInfo.notification.data.type === "confirm"
               ? userInfo.notification.data.number + "입니다"
               : "주문하신 제품이 나왔습니다\n카운터로 와주세요"}
